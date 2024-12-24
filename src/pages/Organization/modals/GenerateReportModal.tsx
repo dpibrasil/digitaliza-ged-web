61cc3c1d651d4a52e19a6848670ee173474ce225
Toni Moreira <moreiraatoni@gmail.com>
2024-12-24 08:57:43 -0300

    feat: generate report modal

--- src/pages/Organization/modals/GenerateReportModal.tsx
+++ src/pages/Organization/modals/GenerateReportModal.tsx
@@ -1,18 +1,27 @@
 import { Form } from "@unform/web";
 import { Input, ReactSelectInput } from "../../../components/Input";
 import Modal, { ModalTitle, ModalType } from "../../../components/Modal";
-import {  OrganizationType } from "../../../types/OrganizationTypes";
+import { OrganizationType } from "../../../types/OrganizationTypes";
+import api from "../../../services/api";
+import { useState } from "react";
 
-function GenerateReportModal({organization, ...rest}: ModalType & {organization: OrganizationType})
-{
-    async function handleSubmit()
-    {
+function GenerateReportModal({ organization, ...rest }: ModalType & { organization: OrganizationType }) {
+    const [directories, setDirectories] = useState()
 
+    console.log(directories)
+    async function handleSubmit(payload: any) {
+        const { data } = await api.get(`/organizations/${organization.id}/report`, {
+            params: payload
+        })
+
+        console.log(data)
+
+        return
     }
 
     return <Modal {...rest}>
         <ModalTitle title="Gerar relatório" subtitle="Preencha os campos corretamente abaixo" />
-        <Form onSubmit={() => {}}>
+        <Form onSubmit={handleSubmit}>
             <div className="grid grid-flow-col gap-1">
                 <Input
                     type="datetime-local"
@@ -29,7 +38,7 @@ function GenerateReportModal({organization, ...rest}: ModalType & {organization:
                 name="directories"
                 label="Diretórios"
                 isMulti={true}
-                options={organization.directories.map(d => ({label: d.name, value: d.id}))}
+                options={organization.directories.map(d => ({ label: d.name, value: d.id }))}
             />
             <button className="bg-blue-500 hover:bg-blue-600 rounded p-2 text-sm text-white mb-4 mt-2" onClick={handleSubmit}>Gerar relatório</button>
         </Form>
